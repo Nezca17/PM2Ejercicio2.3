177586670ddc4592eda9b5257cdcf89f6824141f
loida varela <loidavarela@hotmail.es>
2023-07-19 23:15:36 -0600

    agregue librerias

---
+++ PM2Ejercicio2.3/PM2Ejercicio2.3/Controller/AudioBD.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace PM2Ejercicio2.3.Controller
+{
+    class AudioBD
+    {
+    }
+}
---
+++ PM2Ejercicio2.3/PM2Ejercicio2.3/Models/Audio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+
+namespace PM2Ejercicio2.3.Models
+{
+    public class Audio
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string path { get; set; }
+        public string date { get; set; }
+    }
+}
